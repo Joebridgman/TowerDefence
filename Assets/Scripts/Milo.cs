55b3b8071a4191b250711978239daa7689b9f1fa
Joe Bridgman <joebridgman13@gmail.com>
2025-01-18 10:42:34 +0000

    Added Milo

--- Assets/Scripts/Milo.cs
+++ Assets/Scripts/Milo.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Alfie : Cat
+public class Milo : Cat
 {
     public Projectile attackObject;
     public float attackRate;
@@ -12,33 +12,24 @@ public class Alfie : Cat
 
     public override void Update() {
         base.Update();
-        attackTime += Time.deltaTime;
         if (currentTarget != null && currentAttackCooldown < 0) {
             currentAttackCooldown = attackCooldown;
-            isAttacking = true;
-            attackTime = 1;
-        }
-        if (currentAttackCooldown < 1.3) {
-            GetComponent<SpriteRenderer>().sprite = defaultSprite;
+            ThrowWool();
         }
 
-        if (isAttacking && attackCount < 5 && attackTime >= attackRate && currentTarget != null) {  
-            ThrowMouse();
-            attackCount++;
-            attackTime = 0;
-            if (attackCount >= 5) {
-                isAttacking = false;
-                attackCount = 0;
-            }
+        if (gameObject.transform.rotation.z > 0.7 || gameObject.transform.rotation.z < -0.7) {
+            GetComponent<SpriteRenderer>().flipY = true;
+        }
+        else {
+            GetComponent<SpriteRenderer>().flipY = false;
         }
     }
 
-    void ThrowMouse() {
+    void ThrowWool() {
         var newObject = Instantiate(attackObject, gameObject.transform.position, new Quaternion());
         newObject.GetComponent<Rigidbody2D>().AddForce((gameObject.transform.position -
                                                         currentTarget.transform.position).normalized * -attackObject.speed);
-        newObject.GetComponent<Rigidbody2D>().AddTorque(500);
+        
         newObject.currentLifetime = 0;
-        GetComponent<SpriteRenderer>().sprite = attackSprite;
     }
 }
